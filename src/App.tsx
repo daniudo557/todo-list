d131da3d6310ddbcd0054758325299c90ae34d66
Daniel Macedo <vstcedo@Daniel-Macedo.local>
2021-11-12 19:58:21 -0300

    Update App component to call TodoList and TodoCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Button } from '@mui/material';
+import { useState } from "react";
+import TodoCard from "./components/TodoCard";
+import TodoInput from "./components/TodoInput";
+import { Todo } from "./domains/Todo";
+import "./index.css";
 
 const App = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  console.log(todoList);
+
+  const handleRemove = (selectedTodo: Todo) => {
+    const newTodoList = todoList.filter((todo) => todo.id !== selectedTodo.id);
+
+    setTodoList(newTodoList);
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
-      <Button variant="contained">Hello World</Button>
+      <h1>Todo List</h1>
+      <TodoInput
+        submitTodo={(newTodo) => {
+          setTodoList((oldState) => [...oldState, newTodo]);
+        }}
+      />
+      <TodoCard todoList={todoList} onRemove={handleRemove} />
     </>
   );
 };
