400218daa0533f50d6dfea6b205d468273aa3903
Daniel Macedo <daniel.macedo@unesp.br>
2021-11-27 16:47:17 -0300

    Update all todo services

--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -1,25 +1,37 @@
 import { Endpoints } from "src/configs/endpoints";
 import { Todo } from "src/domains/Todo";
-import RequestService, { Response } from "./requestService";
+import RequestService from "./requestService";
 
-export const fetchTodo = (): Promise<Response<Todo[]>> => {
-  return RequestService.get<Todo[]>(Endpoints.TODO).then((respnse) => respnse);
+export const fetchTodo = (): Promise<Todo[]> => {
+  return RequestService.get<Todo[]>(Endpoints.TODO).then((respnse) => {
+    return respnse.data;
+  });
 };
 
-export const createTodo = (todo: Partial<Todo>): Promise<Response<Todo>> => {
+export const findTodo = (todoId: string): Promise<Todo> => {
+  const url = Endpoints.FIND_TODO.replace("{id}", todoId);
+
+  return RequestService.get<Todo>(url).then((respnse) => {
+    return respnse.data;
+  });
+};
+
+export const createTodo = (todo: Partial<Todo>): Promise<Todo> => {
   return RequestService.post<Todo>(Endpoints.CREATE_TODO, todo).then(
-    (respnse) => respnse
+    (respnse) => respnse.data
   );
 };
 
-export const updateTodo = (): Promise<Response<Todo>> => {
-  return RequestService.put<Todo>(Endpoints.UPDATE_TODO).then(
-    (respnse) => respnse
-  );
+export const updateTodo = (todo: Todo): Promise<Todo> => {
+  const todoId = todo.id.toString();
+  const url = Endpoints.DELETE_TODO.replace("{id}", todoId);
+
+  return RequestService.put<Todo>(url, todo).then((respnse) => respnse.data);
 };
 
-export const deleteTodo = (): Promise<Response<string>> => {
-  return RequestService.delete<string>(Endpoints.DELETE_TODO).then(
-    (respnse) => respnse
-  );
+export const deleteTodo = (todo: Todo): Promise<Todo> => {
+  const todoId = todo.id.toString();
+  const url = Endpoints.DELETE_TODO.replace("{id}", todoId);
+
+  return RequestService.delete<Todo>(url).then((respnse) => respnse.data);
 };
