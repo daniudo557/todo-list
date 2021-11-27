24939f59a3e4afdc296578a187464b7db0a589be
Daniel Macedo <daniel.macedo@unesp.br>
2021-11-27 16:54:34 -0300

    Remove unnused states from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Todo } from "./domains/Todo";
 import "./index.css";
 import Wrapper from "./components/Wrapper";
 import Appbar from "./components/Appbar";
@@ -10,32 +8,16 @@ import TodoDetail from "./pages/TodoDetail";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
-
-  const handleRemove = (selectedTodo: Todo) => {
-    const newTodoList = todoList.filter((todo) => todo.id !== selectedTodo.id);
-
-    setTodoList(newTodoList);
-  };
-
-  const handleSubmitTodo = (newTodo: Todo) => {
-    setTodoList((oldState) => [...oldState, newTodo]);
-  };
-
   return (
     <>
       <Appbar />
       <Wrapper>
         <Switch>
           <Route path="/" exact>
-            <Welcome
-              todoList={todoList}
-              handleSubmitTodo={handleSubmitTodo}
-              handleRemove={handleRemove}
-            />
+            <Welcome />
           </Route>
           <Route path="/todo/:id">
-            <TodoDetail todoList={todoList} handleRemove={handleRemove} />
+            <TodoDetail />
           </Route>
           <Route path="*">
             <NotFound />
