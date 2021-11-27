031c9dac995024da64f84bde1dcfa4845be87000
Daniel Macedo <daniel.macedo@unesp.br>
2021-11-27 16:52:31 -0300

    Update todoCard to get make request to updated status and remove todo

--- src/components/TodoCard/index.tsx
+++ src/components/TodoCard/index.tsx
@@ -1,29 +1,55 @@
 import DeleteIcon from "@mui/icons-material/Delete";
+
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Checkbox,
+  Stack,
   Tooltip,
   Typography,
 } from "@mui/material";
+import { useCallback, useMemo } from "react";
 import { Link } from "react-router-dom";
-import { Todo } from "src/domains/Todo";
+import { Todo, TodoStatus } from "src/domains/Todo";
+import { useTodo } from "src/hooks/useTodo";
+import StatusFlag from "../StatusFlag";
 
 import "./TodoCard.scss";
 
 interface TodoCardProps {
-  onRemove: (todo: Todo) => void;
   todo: Todo;
   type?: "small" | "large";
 }
 
-const TodoCard = ({ onRemove, todo, type = "small" }: TodoCardProps) => {
-  const handleRemove = () => onRemove(todo);
+const TodoCard = ({ todo, type = "small" }: TodoCardProps) => {
+  const { removeTodo, updateTodo } = useTodo();
+
+  const isPending = useMemo(
+    () => todo.status === TodoStatus.PENDING,
+    [todo.status]
+  );
+
+  const toggleTodoStatus = useCallback(
+    () => (isPending ? TodoStatus.COMPLETED : TodoStatus.PENDING),
+    [isPending]
+  );
+
+  const handleRemove = useCallback(() => removeTodo(todo), [removeTodo, todo]);
+
+  const handleUpdate = useCallback(() => {
+    const newStatus = toggleTodoStatus();
+
+    const newTodo: Todo = { ...todo, status: newStatus };
+
+    updateTodo(newTodo);
+  }, [todo, toggleTodoStatus, updateTodo]);
 
   const SmallCard = () => (
     <div className="card-content">
+      <Checkbox defaultChecked={!isPending} onClick={handleUpdate} />
       <Tooltip title="See details">
         <Button
           component={Link}
@@ -31,7 +57,12 @@ const TodoCard = ({ onRemove, todo, type = "small" }: TodoCardProps) => {
           className="card-link"
           color="primary"
         >
-          <p className="label">{todo.label}</p>
+          <p
+            className="label"
+            style={{ textDecoration: isPending ? "none" : "line-through" }}
+          >
+            {todo.title}
+          </p>
         </Button>
       </Tooltip>
 
@@ -52,19 +83,28 @@ const TodoCard = ({ onRemove, todo, type = "small" }: TodoCardProps) => {
         image="https://images.pexels.com/photos/1226398/pexels-photo-1226398.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {todo.label}
-        </Typography>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ textDecoration: isPending ? "none" : "line-through" }}
+          >
+            {todo.title}
+          </Typography>
+          <StatusFlag isPending={isPending} />
+        </Stack>
+
         <Typography variant="body2" color="text.secondary">
           {todo.description}
         </Typography>
       </CardContent>
       <CardActions>
         <Button size="small" onClick={handleRemove}>
-          Complete Todo
-        </Button>
-        <Button size="small" onClick={handleRemove}>
           Cancel Todo
+        </Button>
+        <Button size="small" onClick={handleUpdate}>
+          Complete Todo
         </Button>
       </CardActions>
     </>
