3469a6b87a435fdb4798ad5f5787f2f11198393e
Daniel Macedo <smacedo.daniel@gmail.com>
2022-02-04 00:14:46 -0300

    Include BrowserRouter on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import "./index.css";
-import Wrapper from "./components/Wrapper";
+import { BrowserRouter } from "react-router-dom";
 import Appbar from "./components/Appbar";
 import Footer from "./components/Footer";
 import Router from "./components/Router/Router";
+import Wrapper from "./components/Wrapper";
+import "./index.css";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Appbar />
       <Wrapper>
         <Router />
       </Wrapper>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
